9f2be731486483d5af3f805c380fd4bc03e0d9c7
NgoniMujuru <u16285256@tuks.co.za>
2020-05-20 19:25:03 +0200

    chore: removed references to unnecessary packages in home controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Dynamic;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Locate_closest_business.Models;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 
