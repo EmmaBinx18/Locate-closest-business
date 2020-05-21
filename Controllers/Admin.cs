9e1ef9255bc6479fba52127bd97f1ea4fdd278dd
Emma Coetzer <emmac@bbd.co.za>
2020-05-21 16:55:19 +0200

    feat: added removeAdmin, deny, and confirm buttons

--- Controllers/Admin.cs
+++ Controllers/Admin.cs
@@ -21,8 +21,29 @@ namespace Locate_closest_business.Controllers
         {
             UserManagementModel model = new UserManagementModel();
             model.NewAdmin = new UserModel();
-            model.AdminUsers = new List<UserModel>(); //TODO: add admins to list
+            model.AdminUsers = new List<UserModel>(); //TODO: populate list with admin users
+
+            UserModel user = new UserModel();
+            user.FirstName = "Emma";
+            user.LastName = "Coetzer";
+            user.Email = "emmac@bbd.co.za";
+            user.Phone = "0718831926";
+             
+            model.AdminUsers.Add(user);
+            model.AdminUsers.Add(user);
+            model.AdminUsers.Add(user);
+            model.AdminUsers.Add(user);
+            model.AdminUsers.Add(user);
+            model.AdminUsers.Add(user);
+            
             return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult RemoveAdmin(string email)
+        {
+            //TODO: remove user
+            return RedirectToAction("Admin");
         }
 
         public IActionResult RegisterBusiness()
@@ -41,20 +62,34 @@ namespace Locate_closest_business.Controllers
         public IActionResult RegistrationRequests()
         {
             List<BusinessModel> model = new List<BusinessModel>();
+            //TODO: populate list with business registration requests
+            // BusinessModel business = new BusinessModel();
+            // business.MemberIds = "";
+            // business.CompanyName = "New Company";
+            // business.RegistrationNumber = "1234567890";
+            // business.Category = "Food Delivery";
+            // business.NumEmployees = "24";
+            // business.Address = "12 Apple Str, Randpark Ridge, Randburg, 2156";
 
-            BusinessModel business = new BusinessModel();
-            business.MemberIds = "";
-            business.CompanyName = "New Company";
-            business.RegistrationNumber = "1234567890";
-            business.Category = "Food Delivery";
-            business.NumEmployees = "24";
-            business.Address = "12 Apple Str, Randpark Ridge, Randburg, 2156";
-
-            model.Add(business);
-            model.Add(business);
-            model.Add(business);
-            model.Add(business);
+            // model.Add(business);
+            // model.Add(business);
+            // model.Add(business);
+            // model.Add(business);
             return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult ConfirmRegistration(string registrationNumber)
+        {
+            //TODO: add business
+            return RedirectToAction("RegistrationRequests");
+        }
+
+        [HttpPost]
+        public IActionResult DenyRegistration(string registrationNumber)
+        {
+            //TODO: remove business registration request
+            return RedirectToAction("RegistrationRequests");
         }
 
         public IActionResult Profile()
