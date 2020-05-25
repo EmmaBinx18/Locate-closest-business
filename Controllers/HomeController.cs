4aa87ddacb472a32ad21ea1750f730852af72b0c
Brandon <hexbrandon@gmail.com>
2020-05-25 22:09:16 +0200

    Finished Maps search code

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -30,24 +30,136 @@ namespace Locate_closest_business.Controllers
             {
                 errorMessage = "External_API_Unreachable"
             };
-            string businessType = "";
+            List<String> businessTypeList = new List<string>();
+            List<String> keywordSearchList = new List<string>();
 
-            if (category == "Agriculture, hunting, forestry, fishing")
+            switch (category)
             {
-                businessType = "hardware_store";
+                case ("Agriculture, hunting, forestry, fishing"):
+                    businessTypeList.Add("hardware_store");
+                    businessTypeList.Add("campground");
+
+                    keywordSearchList.Add("Gun shop");
+                    break;
+                case ("Electricity, Gas, Water supply"):
+                    businessTypeList.Add("gas_station");
+                    businessTypeList.Add("electrician");
+                    
+                    keywordSearchList.Add("Water");
+                    break;
+                case ("Manufacturing"):
+                    keywordSearchList.Add("Manufacturing");
+                    break;
+                case ("Construction and related services"):
+                    businessTypeList.Add("roofing_contractor");
+                    businessTypeList.Add("painter");
+                    
+                    keywordSearchList.Add("Construction");
+                    break;
+                case ("Wholesale, Retail trade, Spaza shops"):
+                    businessTypeList.Add("clothing_store");
+                    businessTypeList.Add("convenience_store");
+                    businessTypeList.Add("department_store");
+                    businessTypeList.Add("electronics_store");
+                    businessTypeList.Add("furniture_store");
+                    businessTypeList.Add("hardware_store");
+                    businessTypeList.Add("home_goods_store");
+                    businessTypeList.Add("jewelry_store");
+                    businessTypeList.Add("shoe_store");
+                    businessTypeList.Add("shopping_mall");
+                    businessTypeList.Add("supermarket");
+                    break;
+                case ("Information and Communication services"):
+                    keywordSearchList.Add("Service Provider");
+                    keywordSearchList.Add("Information and Communication services");
+                    break;
+                case ("Media and Entertainment"):
+                    keywordSearchList.Add("broadcast");
+                    keywordSearchList.Add("tv production");
+                    break;
+                case ("Financial and Business services"):
+                    businessTypeList.Add("accounting");
+
+                    keywordSearchList.Add("Private security");
+                    keywordSearchList.Add("financial");
+                    keywordSearchList.Add("machine rental");
+                    break;
+                case ("Accommodation for quarantine, essential services"):
+                    keywordSearchList.Add("quarantine accommodation");
+                    break;
+                case ("Food services (Deliveries)"):
+                    businessTypeList.Add("meal_delivery");
+                    businessTypeList.Add("meal_takeaway");
+                    break;
+                case ("Transport, Storage, Communication services"):
+                    businessTypeList.Add("airport");
+                    businessTypeList.Add("bus_station");
+                    businessTypeList.Add("transit_station");
+                    businessTypeList.Add("train_station");
+                    businessTypeList.Add("taxi_stand");
+                    keywordSearchList.Add("Communication Service");
+                    break;
+                case ("Mining and Quarrying"):
+                    keywordSearchList.Add("mining and quarrying");
+                    break;
+                case ("Repair and Related Emergency services"):
+                    businessTypeList.Add("electrician");
+                    businessTypeList.Add("plumber");
+                    businessTypeList.Add("locksmith");
+                    businessTypeList.Add("car_repair");
+                    businessTypeList.Add("roofing_contractor");
+                    businessTypeList.Add("police");
+
+                    keywordSearchList.Add("towing");
+                    keywordSearchList.Add("Private security");
+                    break;
+                case ("Supply Chain"):
+                    keywordSearchList.Add("logistic");
+                    keywordSearchList.Add("industrial production");
+                    break;
+                case ("Health, Social and Personal services"):
+                    businessTypeList.Add("dentist");
+                    businessTypeList.Add("doctor");
+                    businessTypeList.Add("drugstore");
+                    businessTypeList.Add("pharmacy");
+                    businessTypeList.Add("pet_store");
+                    businessTypeList.Add("hospital");
+                    businessTypeList.Add("hospital");
+                    businessTypeList.Add("hospital");
+
+                    keywordSearchList.Add("pest control");
+                    keywordSearchList.Add("counselling");
+                    keywordSearchList.Add("old age home");
+                    break;
+                default:
+                    businessTypeList.Clear();
+                    keywordSearchList.Clear();
+                    break;
             }
 
             try
             {
-                Task<MapsResponseWrapperModel> task = Task.Run<MapsResponseWrapperModel>(async () => await PerformNearbySearch(lat,lng,businessType,searchRadius,opennow));
-                task.Wait();
-                MapsResponseWrapperModel APIResponse = task.Result;
+                MapsResponseWrapperModel compoundResponse = new MapsResponseWrapperModel();
+                compoundResponse.results = new List<MapsResponseWrapperModel.MapsNearbySearchResultModel>();
 
-                if (APIResponse == null)
+                foreach (String vBusinessType in businessTypeList)
                 {
-                    return Json(errorJSON);
+                    Task<MapsResponseWrapperModel> task = Task.Run<MapsResponseWrapperModel>(async () => await PerformNearbySearch(lat,lng,vBusinessType,"",searchRadius,opennow));
+                    task.Wait();
+                    MapsResponseWrapperModel APIResponse = task.Result;
+                    compoundResponse.results.AddRange(APIResponse.results);
                 }
-                return Json(APIResponse);
+
+                foreach (String vKeyword in keywordSearchList)
+                {
+                    Task<MapsResponseWrapperModel> task = Task.Run<MapsResponseWrapperModel>(async () => await PerformNearbySearch(lat,lng,"",vKeyword,searchRadius,opennow));
+                    task.Wait();
+                    MapsResponseWrapperModel APIResponse = task.Result;
+                    compoundResponse.results.AddRange(APIResponse.results);
+                }
+                compoundResponse.status = "OK";
+
+                return Json(compoundResponse);
             }
             catch (Exception exp)
             {
--- Controllers/HomeControllerHelper.cs
+++ Controllers/HomeControllerHelper.cs
@@ -15,20 +15,43 @@ namespace Locate_closest_business.Controllers
 {
     public partial class HomeController : Controller
     {
-        private async Task<MapsResponseWrapperModel> PerformNearbySearch(string pLat, string pLng, string pCategory, int pSearchRadius, bool pOpenNow)
+        private async Task<MapsResponseWrapperModel> PerformNearbySearch(string pLat, string pLng, string pCategory, string pSearchKeyword, int pSearchRadius, bool pOpenNow)
         {
+            MapsResponseWrapperModel APIResponse;
+
             HttpClient _httpClient = new HttpClient();
             string API_Request = string.Format("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + pLat + "," + 
-                pLng + "&radius=" + pSearchRadius + "&type=" + pCategory + "&key=" + API_Keys.MapsAPI_Key);
+                pLng + "&radius=" + pSearchRadius + "&key=" + API_Keys.MapsAPI_Key);
             if (pOpenNow == true)
             {
                 API_Request += "&opennow=true";
+            }
+            else
+            {
+                
+            }
+
+            if (pCategory != "")
+            {
+                API_Request += ("&type=" + pCategory);
+                Console.WriteLine("Searching for Business type: " + pCategory);
+            }
+            else if (pSearchKeyword != "")
+            {
+                API_Request += ("&keyword=" + pSearchKeyword);
+                Console.WriteLine("Searching for Keyword: " + pSearchKeyword);
+            }
+            else
+            {
+                APIResponse = new MapsResponseWrapperModel();
+                APIResponse.results = new List<MapsResponseWrapperModel.MapsNearbySearchResultModel>();
+                return APIResponse;
             }
 
             var task = await _httpClient.GetAsync(API_Request).ConfigureAwait(false);
             task.EnsureSuccessStatusCode();
             var payload = task.Content.ReadAsStringAsync();
-            MapsResponseWrapperModel APIResponse = JsonConvert.DeserializeObject<MapsResponseWrapperModel>(payload.Result);
+            APIResponse = JsonConvert.DeserializeObject<MapsResponseWrapperModel>(payload.Result);
             
             return APIResponse;
         }
